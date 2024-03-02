62ea55c50ccbd7758b299809d087ef6b8c7169c9
Gabriel Rodrigues Ventura de Araújo <133683181+Brielll2002@users.noreply.github.com>
2024-03-02 01:22:52 -0200

    Add files via upload

---
+++ Server/server.js
@@ -0,0 +1,14 @@
+const http = require('node:http')
+
+const hostname = ''//COLOCAR IP DA MAQUINA
+const port = 3100
+
+const server = http.createServer((req, res)=>{
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World!')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor rodando em: http://${hostname}:${port}`)
+})
